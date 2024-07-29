9dd19dbf93e0063adad70960326ee7704d82b6e4
edmoranm <edanielmoram@gmail.com>
2024-07-28 22:51:59 -0600

    creamos las carpetas con la conexion, funciones, los includes y la primer vista con sus funciones para los clientes

---
+++ includes/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="#">
+    <title>App de Libreria - CRUDJS</title>
+</head>
+
+<!-- <style>
+body {
+    font-family: 'Arial', sans-serif;
+    background: linear-gradient(135deg, #1a237e 0%, #3f51b5 100%);
+    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+    margin: 0;
+    padding: 0;
+    color: #333;
+}
+
+.title-custom {
+    color: #ffffff; Cambia el color del texto a blanco
+}
+
+</style> -->
+
+<body>
+
+<?php include_once 'navbar.php' ?>
---
+++ includes/navbar.php
@@ -0,0 +1,28 @@
+<nav class="navbar navbar-light navbar-expand-lg bg-light">
+    <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarPrincipal" aria-controls="navbarPrincipal" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarPrincipal">
+            <a class="navbar-brand">LIBRERIA EDISA</a>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Mantenimientos
+                    </a>
+                    <ul class="dropdown-menu bg-light">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/libreria_crudjs/views/clientes/index.php">CLIENTES</a>
+                            <a class="nav-link" href="/libreria_crudjs/views/productos/index.php">PRODUCTOS</a>
+                            <a class="nav-link" href="/libreria_crudjs/views/proveedores/index.php">PROVEEDORES</a>
+                            <a class="nav-link" href="/libreria_crudjs/views/ventas/index.php">VENTAS</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
